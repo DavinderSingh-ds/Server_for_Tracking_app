9f87eb74cec023dc1fd555dd1d2479f48f01775d
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-05 18:24:29 +0530

    Defining User Schema or Model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// all the json info passed first then run request handler i.e. authRoutes
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://davinder:davinder@cluster0.ivgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
