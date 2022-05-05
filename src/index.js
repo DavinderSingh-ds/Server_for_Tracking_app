d785567ca6058826f0283428fd2f61ec44637f4f
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-05 18:08:35 +0530

    Postman Request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://davinder:davinder@cluster0.ivgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,11 @@
+// for signup
+const express = require('express');
+
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+    res.send('You made a post request');
+});
+
+
+module.exports = router;
