ca00fd816a7ab3e0b6f63dbda99bc77b9a1ce800
Davinder Singh <79924693+DavinderSingh-ds@users.noreply.github.com>
2022-05-05 17:49:01 +0530

    Connecting Node through Express Api

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoUri = 'mongodb+srv://davinder:davinder@cluster0.ivgka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected',() => {
+    console.log('Connected to Mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo',err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi There!');
+});
+
+// 3000 is port
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
